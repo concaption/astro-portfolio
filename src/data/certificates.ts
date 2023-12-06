72307dd6ee558cdf4358287268c3dca779933244
Usama Navid <concaption@gmail.com>
2023-12-06 21:27:30 +0000

    Update navigation and link styles

---
+++ src/data/certificates.ts
@@ -0,0 +1,21 @@
+import course from "../assets/certificates/Data-Analysis-with-Python-IBM-Coursera-Muhammad-Usama-Navid.webp";
+
+
+export const certficates = [
+  {
+    title: "Learning TypeScript",
+    author: "Josh Goldberg",
+    comment:
+      "Loved it! I learned a lot about TypeScript and how to use it in my projects. I highly recommend it.",
+    img: course,
+    year: 2022,
+    loading: "eager" as const,
+    name: "Generative AI: Foundation Models and Platforms",
+    plateform : "Coursera",
+    date : "November 23, 2023",
+    time : "6 hours",
+    credentialid : "8GVV2Y54SHL2",
+    credentialurl : "https://www.coursera.org/account/accomplishments/verify/8GVV2Y54SHL2",
+    skills: ["Artificial Intelligence (AI)", "Large Language Models (LLM)", "Pre-trained Models","Generative AI", "Foundation Models"],
+  },
+];
